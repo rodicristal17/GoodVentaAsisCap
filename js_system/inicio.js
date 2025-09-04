e6ce8db2e5b849c05e486d4545533ca5f3d13302
rodicristal17 <95529640+rodicristal17@users.noreply.github.com>
2025-09-04 15:04:07 -0300

    Update inicio.js

--- js_system/inicio.js
+++ js_system/inicio.js
@@ -506,7 +506,7 @@ $("div[id=divSaludoGoodSystem]").fadeOut(500);
 	
 }
 
-var codigodeactualizacion="X-GT-1-JMTG-V1.44"
+var codigodeactualizacion="X-GT-1-JMTG-V1.45"
 function controldeactualizacion(codigopc) {	
 	obtener_datos_user()
 	var datos = new FormData();
