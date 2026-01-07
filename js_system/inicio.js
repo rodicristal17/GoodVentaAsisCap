ae995327bfb0d706c99eee89d204ff02a5bad285
Alejandro Alvarez <zorro201021@hotmail.com>
2026-01-07 17:05:52 -0300

    Version de codigo actualizado

--- js_system/inicio.js
+++ js_system/inicio.js
@@ -542,7 +542,7 @@ $("div[id=divSaludoGoodSystem]").fadeOut(500);
 
 
 
-var codigodeactualizacion="X-GT-1-JMTG-V1.52"
+var codigodeactualizacion="X-GT-1-JMTG-V1.53"
 function controldeactualizacion(codigopc) {	
 	obtener_datos_user()
 	var datos = new FormData();
