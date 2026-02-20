ddd18490ba69248a03a645e1683881c3b23220ef
Alejandro Alvarez <zorro201021@hotmail.com>
2026-02-20 07:49:50 -0300

    Renovacion de codigo de actualizacion

--- actualizacion_03022026.sql
+++ actualizacion_03022026.sql
@@ -56,7 +56,7 @@ DELIMITER ;
 
 UPDATE gastos_fijos SET estado="inactivo" WHERE cod_gastos_fijos IS NOT NULL;
 
-UPDATE historialactualizacion SET codigo='X-GT-1-JMTG-V1.61', detalles='Actualizacion php', fecha='2026-02-16' WHERE idhistorialactualizacion= 2;
+UPDATE historialactualizacion SET codigo='X-GT-1-JMTG-V1.62', detalles='Mensajes del sistema implementados', fecha='2026-02-20' WHERE idhistorialactualizacion= 2;
 
 ALTER TABLE interconsulta ADD COLUMN fecha_vencimiento DATE;
 ALTER TABLE interconsulta ADD COLUMN monto_limite INT;
--- js_system/inicio.js
+++ js_system/inicio.js
@@ -596,7 +596,7 @@ $("div[id=divSaludoGoodSystem]").fadeOut(500);
 	
 }
 
-var codigodeactualizacion="X-GT-1-JMTG-V1.61"
+var codigodeactualizacion="X-GT-1-JMTG-V1.62"
 function controldeactualizacion(codigopc) {	
 	obtener_datos_user()
 	var datos = new FormData();
