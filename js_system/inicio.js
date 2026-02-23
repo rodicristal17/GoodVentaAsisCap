75e824d21e97b17dadb880037f138ca68f9cda9e
Alejandro Alvarez <zorro201021@hotmail.com>
2026-02-23 08:01:03 -0300

    Renovacion de codigo de actualizacion

--- js_system/inicio.js
+++ js_system/inicio.js
@@ -596,7 +596,7 @@ $("div[id=divSaludoGoodSystem]").fadeOut(500);
 	
 }
 
-var codigodeactualizacion="X-GT-1-JMTG-V1.62"
+var codigodeactualizacion="X-GT-1-JMTG-V1.63"
 function controldeactualizacion(codigopc) {	
 	obtener_datos_user()
 	var datos = new FormData();
